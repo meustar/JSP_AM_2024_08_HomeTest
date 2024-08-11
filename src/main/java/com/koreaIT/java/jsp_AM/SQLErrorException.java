5559b0c09462e212bcf6e663d977e7dcecdddcbd
meustar <meustar829@gmail.com>
2024-08-11 17:26:38 +0900

    DB연결, 데이터 가져오기.

---
+++ src/main/java/com/koreaIT/java/jsp_AM/SQLErrorException.java
@@ -0,0 +1,14 @@
+package com.koreaIT.java.jsp_AM;
+
+public class SQLErrorException extends RuntimeException {
+    private Exception origin;
+
+    public SQLErrorException(String message, Exception origin) {
+        super(message);
+        this.origin = origin;
+    }
+
+    public Exception getOrigin() {
+        return origin;
+    }
+}
